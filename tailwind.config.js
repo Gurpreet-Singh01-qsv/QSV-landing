6ca6f6c4d9c3ae9322fcdba687c2873cd42d45a8
Gurpreet-Singh01-qsv <gopilubana890@gmail.com>
2025-10-24 23:09:17 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,15 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", "ui-sans-serif", "system-ui"],
+        orbitron: ["var(--font-orbitron)", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
