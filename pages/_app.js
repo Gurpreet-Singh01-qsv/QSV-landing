96df8aed9932263b46dd35655710bf5320da9f8e
Your Name <your.email@example.com>
2025-10-28 20:39:23 +0000

    Update favicon to QSV logo

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <title>QSV - Quantum Speed Ventures</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* Global Favicon - QSV Logo */}
         <link rel="icon" href="/favicon.ico" />
